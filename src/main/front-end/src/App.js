6bf0b4b42132171a479c2d2378216eef37bdf64b
jonghyeon <hyn4956@gmail.com>
2022-11-11 21:00:05 +0900

    react component study 01

--- src/main/front-end/src/App.js
+++ src/main/front-end/src/App.js
@@ -1,29 +1,54 @@
+import React, {Component} from "react";
 import './App.css';
-import React, {useState, useEffect} from 'react';
-import logo from "./logo.svg";
+import TOC from './components/TOC';
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
-function App() {
-    const [message, setMessage]=useState([]);
-    useEffect(()=>{
-        fetch("/hello")
-            .then((res)=>{
-                return res.json();
-            })
-            .then((data)=>{
-                setMessage(data);
-            });
-    },[]);
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                {/*기본코드*/}
-                <ul>
-                    {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
-                </ul>
-            </header>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: 'welcome',
+            subject: {title:'WEB', suj:'World Wide Web!'},
+            welcome: {title:'Welcome', desc:'Hello, React!!'},
+            contents: [
+                {id:1, title:'HTML', desc:'HTML is for information'},
+                {id:2, title:'CSS', desc:'CSS is for design'},
+                {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+            ]
+        }
+    }
+    render() {
+        console.log('App render');
+        var _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        }else if(this.state.mode === 'read'){
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
+        return (
+            <div className="App">
+                <header>
+                    <h1><a href="/" onClick={function (e){
+                        console.log(e);
+                        debugger;
+                        alert("hi");
+                    }}>{this.state.subject.title}</a></h1>
+                    {this.state.subject.suj}
+                </header>
+                {/*<Subject*/}
+                {/*    title={this.state.subject.title}*/}
+                {/*    sub={this.state.subject.suj}*/}
+                {/*>*/}
+                {/*</Subject>*/}
+                {/*<Subject title="React" sub="For UI"></Subject>*/}
+                <TOC data={this.state.contents}></TOC>
+                <Content title={_title} desc={_desc}></Content>
+            </div>
+        );
+    }
 }
 
 export default App;
